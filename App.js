3965d3068a1292999e5191a7532c24a34bd6a3d7
Okan Aktaş <okanaktas02@gmail.com>
2020-06-04 22:24:32 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your yeter artık!</Text>
+      <Text>MFE Başlangıç</Text>
     </View>
   );
 }
