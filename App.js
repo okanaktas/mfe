3f26699faa4f73cb0df7094cb66234b88b7e589d
Okan Aktaş <okanaktas02@gmail.com>
2020-06-04 22:18:00 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your appasdasd!</Text>
+      <Text>Open up App.js to start working on your app sokacam artık ha!</Text>
     </View>
   );
 }
