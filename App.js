ef1da55520ae7f98ebf2537a902d086fcb44ea86
FIRAT DOĞAN <firatdogan0@gmail.com>
2020-06-04 22:37:17 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,7 @@ import styles from './style';
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< Updated upstream
       <Text>MFE</Text>
-=======
-      <Text>MFE hoş geldiniz..  xD</Text>
->>>>>>> Stashed changes
     </View>
   );
 }
