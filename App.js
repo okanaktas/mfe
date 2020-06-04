f97fc48961e66a745a6f5a96fd91800e1f5d51d7
FIRAT DOĞAN <firatdogan0@gmail.com>
2020-06-04 22:58:58 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import styles from './style';
+const styles = require('./style');
 
 export default function App() {
   return (
